a8134ec17a83f1caa1af13415dfa0aa5d3614614
james8bond <15974708423@139.com>
2020-12-16 20:40:13 +0800

    环境搭建 V0.04

---
+++ application/api/controller/Bourne.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *   User:   邦德
+ *   Date:   2020/12/16 : 20:39
+ */
+
+
+namespace app\api\controller;
+
+
+class Bourne
+{
+    public function index()
+    {
+        return 'Hello Bourne !!';
+    }
+}
