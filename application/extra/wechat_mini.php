df0b6d77f36697561b5dd7a7af5023007e11ffb6
james8bond <15974708423@139.com>
2021-03-09 12:58:31 +0800

    公共配置 添加小程序配置文件 wechat_mini

---
+++ application/extra/wechat_mini.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *   User:   邦德
+ *   Date:   2021/3/9 : 12:51
+ */
+return [
+    // My风袖 小程序
+    'fengxiu' => [
+        'appid'     => 'wx58b7139d6d6883df',
+        'appsecret' => '59e266746f117d634936e02cfec1a335',
+    ],
+
+    // 儒儒家 小程序 
+    'rurujia' => [
+        'appid'     => 'wxcdc9699ac01c1f23',
+        'appsecret' => 'ea3a61914767faf0877545623fa9b60d',
+    ],
+];
+
