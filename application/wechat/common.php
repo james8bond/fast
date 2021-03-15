4529aa91ac61d4b1ada3978a71ea34c9b519428c
james8bond <15974708423@139.com>
2021-03-15 16:12:22 +0800

    配置wechat模块的配置文件

--- application/wechat/common.php
+++ application/wechat/common.php
@@ -1,1 +1,26 @@
 <?php
+if (!function_exists('get_easywechat_app')) {
+    /**
+     * 获取EasyWeChat对象
+     */
+    function get_easywechat_app($wechat = [])
+    {
+        if (empty($wechat)) {
+            exception('小程序配置信息不能为空');
+        }
+        return EasyWeChat\Factory::miniProgram($wechat);
+    }
+}
+
+if (!function_exists('get_easywechat_payment')) {
+    /**
+     * 获取EasyWeChat对象
+     */
+    function get_easywechat_payment($wechat = [])
+    {
+        if (empty($wechat)) {
+            exception('小程序配置信息不能为空');
+        }
+        return EasyWeChat\Factory::miniProgram($wechat);
+    }
+}
