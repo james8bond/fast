48cbe78d7bb4269f9bca7466023f2a7035120aab
james8bond <15974708423@139.com>
2021-03-15 16:03:02 +0800

    添加微信商户号,商户秘钥, 回调地址

--- application/extra/wechat_mini.php
+++ application/extra/wechat_mini.php
@@ -6,14 +6,27 @@
 return [
     // My风袖 小程序
     'fengxiu' => [
-        'appid'     => 'wx58b7139d6d6883df',
-        'appsecret' => '59e266746f117d634936e02cfec1a335',
+        'app_id'        => 'wx58b7139d6d6883df',
+        'secret'        => '59e266746f117d634936e02cfec1a335',
+        'response_type' => 'array',
+        'log'           => [
+            'level' => 'debug',
+            'file'  => RUNTIME_PATH . 'wechat.log',
+        ],
     ],
 
     // 儒儒家 小程序 
     'rurujia' => [
-        'appid'     => 'wxcdc9699ac01c1f23',
-        'appsecret' => 'ea3a61914767faf0877545623fa9b60d',
+        'app_id'        => 'wxcdc9699ac01c1f23',
+        'secret'        => 'ea3a61914767faf0877545623fa9b60d',
+        'response_type' => 'array',
+        'log'           => [
+            'level' => 'debug',
+            'file'  => RUNTIME_PATH . 'wechat.log',
+        ],
+        'mch_id'        => 'your-mch-id',           // 商户号
+        'key'           => 'key-for-signature',     // 商户号秘钥
+        'notify_url'    => '',                      // 回调地址
     ],
 ];
 
